b1876b0d4413ca35cc801c9f15fc33e4714111f6
williamtn4 <williamtn4@gmail.com>
2025-12-03 20:37:58 -0600

    More work on Day 2 problem

--- AoC.py
+++ AoC.py
@@ -41,30 +41,57 @@ hit = np.zeros(N, dtype=int)
 for i in range(N):
 	dial_value, hit[i] = move_dial(dial_value,rotation_directions[i],dial_distance[i])
 passcode = sum(hit)
-<<<<<<< HEAD
-print("Passcode for secret entrance: {}".format(passcode))
-
-# --- Day 2 ---
-=======
 # print("Passcode for secret entrance: {}".format(passcode))
 
 # --- Day 2 ---
-number = '238238238'
+number = '138138138138'
+print(number)
 def find_invalid(number: str):
 	n = len(number)
-	if np.mod(n,2) == 1 and all(np.isin(number,number[0])):
+	arr = [char for char in number]
+	invalid = 0
+	
+	digit_factors = set()
+	i = 1
+	while i*i <= n:
+		if n % i == 0:
+			digit_factors.add(i)
+			digit_factors.add(n // i)
+		i += 1
+	digit_factors = list(digit_factors)
+	print(digit_factors)
+	max_char_len = max(digit_factors)
+	digit_factors = digit_factors[1:-1]
+	N = len(digit_factors)-1
+	print(digit_factors)
+	if np.mod(n,2) == 1 and all(np.isin(arr,arr[0])):
 		invalid = int(number)
 	else:
-		arr = [char for char in number]
-		while len(arr) > 2:
-			arr = [arr[i] + arr[i+1] for i in range(0,len(arr),2)]
-			print(arr)
-			if all(np.isin(arr,arr[0])):
-				invalid = int(number)
-			else:
-				pass
+		new_arr = []
+		for i in range(N):
+			char = ''
+			for j in range(n // ):
+
+			new_arr.append()
+			
+	# elif np.mod(n,3) == 0:
+	# 	while len(arr) > 2:
+	# 		new_arr = []
+	# 		for i in range(0,len(arr),3):
+
+	# 		arr = [arr[i] + arr[i+1] + arr[i+2] for i in range(0,len(arr),3)]
+	# 		if all(np.isin(arr,arr[0])):
+	# 			invalid = int(number)
+	# 		else:
+	# 			pass
+	# else:
+	# 	while len(arr) > 2:
+	# 		arr = [arr[i] + arr[i+1] for i in range(0,len(arr),2)]
+	# 		if all(np.isin(arr,arr[0])):
+	# 			invalid = int(number)
+	# 		else:
+				# pass
 	return invalid
 
 invalid = find_invalid(number)
 print(invalid)
->>>>>>> ab28d76f11543ae5700c6785264727ba7e47fd78
