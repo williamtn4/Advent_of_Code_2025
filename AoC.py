4e20b8297df5d7f851f0bfbaeba27a790e581805
William Newman <williamtn4@gmail.com>
2025-12-03 22:22:52 -0600

    More work on Day 2 No Success - Need to make multiple .py's for each day

--- AoC.py
+++ AoC.py
@@ -44,54 +44,54 @@ passcode = sum(hit)
 # print("Passcode for secret entrance: {}".format(passcode))
 
 # --- Day 2 ---
-number = '138138138138'
-print(number)
-def find_invalid(number: str):
-	n = len(number)
+
+def find_invalid(number,repeat_times):
+	number = str(number)
 	arr = [char for char in number]
 	invalid = 0
-	
-	digit_factors = set()
-	i = 1
-	while i*i <= n:
-		if n % i == 0:
-			digit_factors.add(i)
-			digit_factors.add(n // i)
-		i += 1
-	digit_factors = list(digit_factors)
-	print(digit_factors)
-	max_char_len = max(digit_factors)
-	digit_factors = digit_factors[1:-1]
-	N = len(digit_factors)-1
-	print(digit_factors)
-	if np.mod(n,2) == 1 and all(np.isin(arr,arr[0])):
+	n = len(arr)
+
+	if np.mod(n,2) == 1:
+		exit
+	elif all(np.isin(arr,arr[0])):
 		invalid = int(number)
 	else:
-		new_arr = []
-		for i in range(N):
-			char = ''
-			for j in range(n // ):
-
-			new_arr.append()
-			
-	# elif np.mod(n,3) == 0:
-	# 	while len(arr) > 2:
-	# 		new_arr = []
-	# 		for i in range(0,len(arr),3):
-
-	# 		arr = [arr[i] + arr[i+1] + arr[i+2] for i in range(0,len(arr),3)]
-	# 		if all(np.isin(arr,arr[0])):
-	# 			invalid = int(number)
-	# 		else:
-	# 			pass
-	# else:
-	# 	while len(arr) > 2:
-	# 		arr = [arr[i] + arr[i+1] for i in range(0,len(arr),2)]
-	# 		if all(np.isin(arr,arr[0])):
-	# 			invalid = int(number)
-	# 		else:
-				# pass
+		digit_factors = set()
+		i = 1
+		while i*i <= n:
+			if n % i == 0:
+				digit_factors.add(i)
+				digit_factors.add(n // i)
+			i += 1
+		if repeat_times != None:
+			for i,factor in enumerate(digit_factors):
+				if n/repeat_times == factor:
+					digit_factors = [factor]
+				else:
+					digit_factors = sorted(digit_factors)
+					digit_factors = digit_factors[1:-1]
+		N = len(digit_factors)
+		
+		for j in range(N):
+			combined = [''.join(arr[i:i + digit_factors[j]]) for i in range(0,len(arr),digit_factors[j])]
+			if all(np.isin(combined,combined[0])):
+				invalid = int(number)
+			else:
+				exit
 	return invalid
 
-invalid = find_invalid(number)
-print(invalid)
+with open('input_d2.txt','r') as f:
+	line = f.readline()
+	ID_ranges = line.split(',')
+
+invalid_list = []
+for i,ID_range in enumerate(ID_ranges):
+	start = int(ID_range.split('-')[0])
+	stop = int(ID_range.split('-')[-1])
+	for number in range(start,stop+1):
+		invalid = find_invalid(number,2)
+		if invalid != 0:
+			invalid_list.append(invalid)
+
+invalid_sum = np.sum(invalid_list)
+print(invalid_sum)
